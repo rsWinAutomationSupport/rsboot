01cf7167e07acad37d895d44d9c82e3876c6d611
Mark Key <mark.key@rackspace.com>
2015-04-22 10:17:22 -0500

    updated schtask

--- boot.ps1
+++ boot.ps1
@@ -229,7 +229,7 @@ Configuration Boot {
           return $true
         }
         else {
-          schtasks.exe /Create /SC daily /RI 15 /TN "\Microsoft\Windows\Desired State Configuration\Consistency" /RU System /F /TR "PowerShell.exe -NonInt -Window Hidden -Command 'Invoke-CimMethod -Namespace root/Microsoft/Windows/DesiredStateConfiguration -Cl MSFT_DSCLocalConfigurationManager -Method PerformRequiredConfigurationChecks -Arguments @{Flags = [System.UInt32]2}'"
+          schtasks.exe /Create /sc Minute /mo 15 /TN "\Microsoft\Windows\Desired State Configuration\Consistency" /RU System /F /TR "PowerShell.exe -NonInt -Window Hidden -Command 'Invoke-CimMethod -Namespace root/Microsoft/Windows/DesiredStateConfiguration -Cl MSFT_DSCLocalConfigurationManager -Method PerformRequiredConfigurationChecks -Arguments @{Flags = [System.UInt32]2}'"
         }
       }
 
