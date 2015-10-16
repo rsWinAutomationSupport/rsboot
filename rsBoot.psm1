9d4fd20719de0d0c17ed8d63886d09d3c075eb19
Alexei Andreyev <alexei.andreyev@gmail.com>
2015-10-16 14:11:48 +0100

    Module install and pre-bootstrap logic

--- rsBoot.psm1
+++ rsBoot.psm1
@@ -1,6 +1,34 @@
-﻿# Import all local dependencies
-Get-Item (Join-Path -Path $PSScriptRoot -ChildPath 'Scripts\*.ps1') | 
+﻿# Import all local functions dependencies
+Get-Item (Join-Path -Path $PSScriptRoot -ChildPath 'functions\*.ps1') | 
     ForEach-Object {
         Write-Verbose ("Importing sub-module {0}." -f $_.FullName)
         . $_.FullName
     }
+
+# Executes pre-DSCbootstrap scripts from module scripts folder
+function Invoke-PreBootScript
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Hashtable that contains filename of the script to run and any parameters it requires: @{"script.ps1" = "-param test"}
+        [hashtable] $Scripts
+    )
+    
+    $ScriptPath = $(Join-Path -Path $PSScriptRoot -ChildPath "scripts")
+
+    ForEach ($item in $scripts.GetEnumerator())
+    {
+        $Script = $item.Name
+        $Parameters = $item.Value
+        
+        Write-Verbose "Executing script: $script"
+        & $(Join-Path -Path $ScriptPath -ChildPath $Script) @Parameters
+    }
+}
+
+# Executes main Boot configuration of the DSC Bootstraping process
+function Invoke-DSCBoot
+{
+
+}
