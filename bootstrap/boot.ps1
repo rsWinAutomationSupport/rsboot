d09f48c3d02609614f9ad789138711d3d3573d2b
Alexei Andreyev <alexei.andreyev@rackspace.co.uk>
2015-10-18 21:50:29 +0100

    Adding latest updates to boot and module functions

--- bootstrap/boot.ps1
+++ bootstrap/boot.ps1
@@ -4,7 +4,7 @@
 .DESCRIPTION
    Bootstrap script for initialising a DSC Pull server or one of its clients.
 .EXAMPLE
-   boot.ps1 -BootParameters @{} -Verbose
+   boot.ps1 -PullConfig "rsPullServer.ps1" -BootParameters @{} -Verbose
 
    Bootstrap a DSC Pull server
 .EXAMPLE
@@ -26,6 +26,14 @@ Param
 
     # BootModuleZipURL - Link to the Zip file for the Bootstrap module
     [string] $BootModuleZipURL = "https://github.com/rsWinAutomationSupport/rsboot/archive/ModulePOC.zip",
+
+    # ModuleName - Name of the main bootstrap module
+    [string] $ModuleName = "rsBoot",
+
+    # File name of DSC configuration file of pull server
+    [string] $PullConfig,
+
+    [int] $PullPort = 8080,
 
     # BootParameters - Hashtable that contains bootstrap parameters for the type of server being built
     [hashtable] $BootParameters
@@ -52,7 +60,7 @@ Write-Verbose "Setting LocalMachine execurtion policy to RemoteSigned"
 Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy RemoteSigned -Force
 
 Write-Verbose "Setting environment variables"
-[Environment]::SetEnvironmentVariable('defaultPath',$defaultPath,'Machine')
+[Environment]::SetEnvironmentVariable('defaultPath',$DefaultPath,'Machine')
 [Environment]::SetEnvironmentVariable('nodeInfoPath',$NodeInfoPath,'Machine')
 
 Write-Verbose " - DefaultPath location: $DefaultPath"
@@ -90,21 +98,23 @@ Set-ACL $defaultPath $objACL
 if (($BootParameters -eq $null) -or ($BootParameters -eq ''))
 {
     Write-Verbose "BootParameters were not provided, checking local secrets.."
-    if (Test-Path "$DefaultPath/secrets.json")
+    if (Test-Path "$DefaultPath\BootParameters.xml")
     {
-        Write-Verbose "Reading contents of $DefaultPath/secrets.xml"
-        $BootParameters = Import-Clixml "$DefaultPath/secrets.xml"
+        Write-Verbose "Reading contents of $DefaultPath\BootParameters.xml"
+        $BootParameters = Import-Clixml "$DefaultPath\BootParameters.xml"
     }
     else
     {
-        Throw "BootParameters were not provided and secrets.json not found!"
+        Throw "BootParameters were not provided and BootParameters.xml not found!"
+        exit
     }
 }
 else
 {
-    Write-Verbose "Saving boot parameters to $DefaultPath\secrets.xml"
-    $BootParameters | Export-Clixml -Path "$DefaultPath\secrets.xml" -Force
+    Write-Verbose "Saving boot parameters to $DefaultPath\BootParameters.xml"
+    $BootParameters | Export-Clixml -Path "$DefaultPath\BootParameters.xml" -Force
 }
+
 #endregion
 
 #########################################################################################################
@@ -133,12 +143,24 @@ Register-ScheduledTask DSCBoot -InputObject $Task
 #########################################################################################################
 # Download & install rsBoot module
 #region##################################################################################################
+$Target = "github.com"
+Write-Verbose "Checking connectivity to '$Target'..."
+if (-not (Test-Connection $Target -Quiet))
+{
+    do
+    {
+        Write-Host "Waiting for network connectivity to '$Target' to be established..."
+        Sleep -Seconds 20
+    }
+    until (-not (Test-Connection $Target -Quiet))
+}
+
+<#
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 $WinTemp = "$env:windir\Temp"
 $ModuleFileName = $BootModuleZipURL.Split("/")[-1]
 $ZipPath = "$WinTemp\$ModuleFileName"
 
-$ModuleName = "rsBoot"
 $PSModuleLocation = "$Env:ProgramFiles\WindowsPowerShell\Modules\"
 
 Write-Verbose "Downloading the Bootstrap PS module"
@@ -164,9 +186,81 @@ if (Test-Path "$PSModuleLocation\$ModuleName")
 }
 Write-Verbose "Installing rsBoot module"
 Move-Item -Path "$WinTemp\$ModuleName" -Destination $PSModuleLocation
-
+#>
 Write-Verbose "Importing $ModuleName module"
-Import-Module -Name $ModuleName
+Import-Module -Name $ModuleName -Force -Verbose
 #endregion
 
+#########################################################################################################
+# Execute pre-bootstrap scripts
+#region##################################################################################################
+if ($BootParameters.PreBoot -ne $null)
+{
+    Invoke-PreBootScript -Scripts $BootParameters.PreBoot -Verbose
+}
+#endregion
+
+#########################################################################################################
+# Execute main bootstrap process
+#region##################################################################################################
+# Determine if we're building a Pull server or a client
+if ($PullServerConfig -ne $null)
+{
+    Write-Verbose "Initiating DSC Pull Server bootstrap..."
+    if(!($PullServerAddress))
+    {
+        $PullServerAddress = $env:COMPUTERNAME
+    }
+    # Need $pullserver_config parameter/variable
+    Write-Secrets -PullServerAddress $PullServerAddress -pullserver_config $PullServerConfig `
+                  -BootParameters $BootParameters -Path $DefaultPath
+    Enable-WinRM
+
+    <#
+    
+    
+    # Boot -PullServerAddress $PullServerAddress -OutputPath $defaultPath -Verbose
+    # Start-DscConfiguration -Force -Path $defaultPath -Wait -Verbose
+    Set-LCM
+    #PullServer - Run RsPlatform and then PullServer config file
+    if($secrets){
+        Set-rsPlatform
+        Set-Pull -pullserver_config $pullserver_config
+    }#>
+}
+
+<#
+else
+{
+    Write-Verbose "Initiating DSC Client bootstrap..."
+
+    # Check if PullServerAddress provided is an IP
+    if($PullServerAddress -notmatch '[a-zA-Z]')
+    {
+        $PullServerAddress = Get-PullServerInfo -PullServerAddress $PullServerAddress -PullPort $PullPort
+    }
+
+    
+    # Get-NICInfo
+    Enable-WinRM
+    # Boot -PullServerAddress $PullServerAddress -OutputPath $defaultPath -Verbose
+    # Start-DscConfiguration -Force -Path $defaultPath -Wait -Verbose
+    Set-LCM
+    Start-DscConfiguration -UseExisting -Wait -Force -Verbose
+}
+#>
+#endregion
+
+
+
+
+
+
+if (Get-ScheduledTask -TaskName 'DSCBoot' -ErrorAction SilentlyContinue)
+{
+    Write-Verbose "Removing the 'DSCBoot' task..."
+    Unregister-ScheduledTask -TaskName DSCBoot -Confirm:$false
+}
 #Stop-Transcript
+
+
--- rsBoot.psm1
+++ rsBoot.psm1
@@ -14,16 +14,22 @@ function Invoke-PreBootScript
         # Hashtable that contains filename of the script to run and any parameters it requires: @{"script.ps1" = "-param test"}
         [hashtable] $Scripts
     )
-    
-    $ScriptPath = $(Join-Path -Path $PSScriptRoot -ChildPath "scripts")
 
+    $ScriptPath = $(Join-Path -Path $PSScriptRoot -ChildPath "scripts")
     ForEach ($item in $scripts.GetEnumerator())
     {
         $Script = $item.Name
         $Parameters = $item.Value
-        
-        Write-Verbose "Executing script: $script"
-        & $(Join-Path -Path $ScriptPath -ChildPath $Script) @Parameters
+        $FullScriptPath = $(Join-Path -Path $ScriptPath -ChildPath $Script)
+        if (Test-Path $FullScriptPath)
+        {
+            Write-Verbose "Executing script: $script"
+            & $FullScriptPath @Parameters
+        }
+        else
+        {
+            Write-Verbose "Script '$Script' was not found at $ScriptPath"
+        }
     }
 }
 
@@ -34,7 +40,7 @@ function Get-PullServerInfo
     param
     (
         [string] $PullServerAddress,
-        [int] $PullServerPort,
+        [int] $PullPort,
         [int] $SleepSeconds = 10
     )
 
@@ -73,7 +79,59 @@ function Get-PullServerInfo
 }
 
 # Executes main Boot configuration of the DSC Bootstraping process
-function Invoke-DSCBoot
+function Enable-WinRM
 {
-    
+    if( (Get-ChildItem WSMan:\localhost\Listener | Where-Object Keys -eq "Transport=HTTP").count -eq 0 )
+    {
+        New-WSManInstance -ResourceURI winrm/config/Listener -SelectorSet @{Address="*";Transport="http"}
+    }
 }
+
+#Creates secrets.json on a pullserver
+function Write-Secrets 
+{
+    param
+    (
+        # Pull server address
+        [string] $PullServerAddress,
+
+        # Name of the DSC script file that contains main Pull server config
+        [string] $pullserver_config,
+
+        # Contents of the secrets file
+        [hashtable] $BootParameters,
+        
+        # Path where to store the secrets
+        [string] $Path
+    )
+
+    Write-Verbose "Checking that  all compulsory secrets keys are present..."
+    $keys = @('branch_rsConfigs', 'mR', 'git_username', 'gitBr', 'git_oAuthtoken','shared_key')
+    ForEach($key in $keys)
+    {
+        if($BootParameters.keys -notcontains $key)
+        { 
+            Write-Verbose "$key key is missing from BootParameters"
+            exit
+        }
+    }
+
+    Write-Verbose "Preparing secrets and exporting as json file"
+    # Add additional keys to the boot parameters, which were passed directly to the boot.ps1 script
+    $BootParameters.Add("PullServerAddress","$PullServerAddress")
+    $BootParameters.Add("pullserver_config","$pullserver_config")
+    # Remove redundant keys
+    $BootParameters.Remove("PreBoot")
+    
+    $SecretsPath = (Join-Path $Path 'secrets.json')
+    # Make a backup in case of there being an existing secrets file
+    if (Test-Path $SecretsPath)
+    {
+        Write-Verbose "Existing secrets.json found - making a backup..."
+        $TimeDate = (Get-Date -Format ddMMMyyyy_hhmmss).ToString()
+        Move-Item $SecretsPath -Destination (Join-Path $Path "secrets.json.$TimeDate.bak") -Force
+    }
+    Write-Verbose "Writing secrets.json"
+    Set-Content -Path $SecretsPath -Value $($BootParameters | ConvertTo-Json -Depth 4)
+}
+
