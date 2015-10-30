557584d61493beae34293a7233c452bc65cc1cc5
Alexei Andreyev <alexei.andreyev@rackspace.co.uk>
2015-10-30 15:26:14 +0000

    Adding Pull server boot DSc configuration Change PullServerConfig parameter for readability

--- bootstrap/boot.ps1
+++ bootstrap/boot.ps1
@@ -31,7 +31,7 @@ Param
     [string] $ModuleName = "rsBoot",
 
     # File name of DSC configuration file of pull server
-    [string] $PullConfig,
+    [string] $PullServerConfig,
 
     [int] $PullPort = 8080,
 
@@ -42,7 +42,268 @@ Param
 #########################################################################################################
 # Helper functions
 #region##################################################################################################
+Configuration PullBoot
+{  
+    param 
+    (
+        [hashtable] $BootParameters,
+        [string] $PullConfigInstallPath
+    )
+    node $env:COMPUTERNAME 
+    {
+        File DevOpsDir
+        {
+            DestinationPath = $PullConfigInstallPath
+            Ensure = 'Present'
+            Type = 'Directory'
+        }
+        Script GetWMF4 
+        {
+            SetScript = {
+                $Uri = 'http://download.microsoft.com/download/3/D/6/3D61D262-8549-4769-A660-230B67E15B25/Windows6.1-KB2819745-x64-MultiPkg.msu'
+                Write-Verbose "Downloading WMF4"
+                Invoke-WebRequest -Uri $Uri -OutFile 'C:\Windows\temp\Windows6.1-KB2819745-x64-MultiPkg.msu' -UseBasicParsing
+            }
 
+            TestScript = {
+                if( $PSVersionTable.PSVersion.Major -ge 4 ) 
+                {
+                    return $true
+                }
+                if( -not (Test-Path -Path 'C:\Windows\Temp\Windows6.1-KB2819745-x64-MultiPkg.msu') ) 
+                {
+                    Write-Verbose "WMF4 Installer not found locally"
+                    return $false
+                }
+                else
+                {
+                    return $true
+                }
+            }
+
+            GetScript = {
+                return @{
+                    'Result' = 'C:\Windows\Temp\Windows6.1-KB2819745-x64-MultiPkg.msu'
+                }
+            }
+            DependsOn = @('[File]DevOpsDir')
+        }
+        Script InstallWMF4 
+        {
+            SetScript = {
+                Write-Verbose "Installing WMF4"
+                Start-Process -Wait -FilePath 'C:\Windows\Temp\Windows6.1-KB2819745-x64-MultiPkg.msu' -ArgumentList '/quiet' -Verbose
+                Write-Verbose "Setting DSC reboot flag"
+                Start-Sleep -Seconds 30
+                $global:DSCMachineStatus = 1 
+            }
+            TestScript = {
+                if($PSVersionTable.PSVersion.Major -ge 4) 
+                {
+                    return $true
+                }
+                else 
+                {
+                    Write-Verbose "Current PowerShell version is lower than the requried v4"
+                    return $false
+                }
+            }
+            GetScript = {
+                return @{'Result' = $PSVersionTable.PSVersion.Major}
+            }
+            DependsOn = '[Script]GetWMF4'
+        }
+        Package InstallGit 
+        {
+            Name = 'Git version 2.6.2'
+            Path = 'https://github.com/git-for-windows/git/releases/download/v2.6.2.windows.1/Git-2.6.2-64-bit.exe'
+            ProductId = ''
+            Arguments = '/VERYSILENT /DIR "C:\Program Files\Git\"'
+            Ensure = 'Present'
+        }
+        Script SetGitPath
+        {
+            SetScript = {
+                if (-not(Test-Path "C:\Program Files\Git\bin"))
+                {
+                    Throw "Git bin folder was not found - check that git client is actualy installed"
+                }
+                $currentPath = ([System.Environment]::GetEnvironmentVariable("Path","Machine")).Split(";")
+                if (-not($currentPath.Contains("C:\Program Files\Git\bin")))
+                {
+                    $env:Path = $env:Path + ";C:\Program Files\Git\bin"
+                    [Environment]::SetEnvironmentVariable( "Path", $env:Path, [System.EnvironmentVariableTarget]::Machine )
+                }
+                else
+                {
+                    Write-Verbose 'Global path variable already contains "C:\Program Files\Git\bin"'
+                }
+            }
+            TestScript = {
+                $env:path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+                return [bool](Get-Command git.exe -ErrorAction SilentlyContinue)
+                 
+            }
+            GetScript = {
+                @{EnvPath = $([System.Environment]::GetEnvironmentVariable("Path","Machine")) }
+            }
+            DependsOn = '[Package]InstallGit'
+        }
+        Script UpdateGitConfig 
+        {
+            SetScript = {
+                $env:path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+                Start-Process -Wait 'git.exe' -ArgumentList "config --system user.email $env:COMPUTERNAME@localhost.local"
+                Start-Process -Wait 'git.exe' -ArgumentList "config --system user.name $env:COMPUTERNAME"
+            }
+            TestScript = {
+                $env:path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+                $GitConfig = & git config --list
+
+                if( [bool]($GitConfig -match $env:COMPUTERNAME) )
+                {
+                    return $true
+                }
+                else
+                {
+                    return $false
+                }
+            }
+            GetScript = {
+                $env:path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+                $GitConfig = & git config --list
+                @{"Result" = $($GitConfig -match $env:COMPUTERNAME)}
+            }
+            DependsOn = '[Script]SetGitPath'
+        }
+        Script Clone_rsConfigs 
+        {
+            SetScript = {
+                $env:path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+                $BootParams = $using:BootParameters
+                $gitArguments = "clone --branch $($BootParams.branch_rsConfigs) https://$($BootParams.git_Oauthtoken)@github.com/$($BootParams.git_username)/$($BootParams.mR).git"
+                Set-Location $using:PullConfigInstallPath -Verbose
+                Start-Process -Wait 'git.exe' -ArgumentList $gitArguments
+            }
+            TestScript = {
+                $BootParams = $using:BootParameters
+                if(Test-Path -Path $(Join-Path $using:PullConfigInstallPath $BootParams.mR)) 
+                {
+                    return $true
+                }
+                else 
+                {
+                    return $false
+                }
+            }
+            GetScript = {
+                $BootParams = $using:BootParameters
+                return @{'Result' = (Test-Path -Path $(Join-Path $using:PullConfigInstallPath $BootParams.mR) -PathType Container)}
+            }
+            DependsOn = '[Script]UpdateGitConfig'
+        }
+        File rsPlatformDir 
+        {
+            SourcePath = (Join-Path $PullConfigInstallPath "$($BootParameters.mR)\rsPlatform")
+            DestinationPath = 'C:\Program Files\WindowsPowerShell\Modules\rsPlatform'
+            Type = 'Directory'
+            Recurse = $true
+            MatchSource = $true
+            Ensure = 'Present'
+            DependsOn = '[Script]Clone_rsConfigs'
+        }
+        Script ClonersPackageSourceManager 
+        {
+            SetScript = {
+                $env:path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+                $BootParams = $using:BootParameters
+                Set-Location 'C:\Program Files\WindowsPowerShell\Modules\'
+                Start-Process -Wait 'git.exe' -ArgumentList "clone --branch $($BootParams.gitBr) https://github.com/rsWinAutomationSupport/rsPackageSourceManager.git"
+            }
+            TestScript = {
+                return ([bool](Get-DscResource rsGit -ErrorAction SilentlyContinue))
+            }
+            GetScript = {
+                return @{'Result' = ([bool] (Get-DscResource rsGit -ErrorAction SilentlyContinue ))}
+            }
+            DependsOn = '[File]rsPlatformDir'
+        }
+        #Creates PullServer Certificate that resides on DSC endpoint
+        Script CreateServerCertificate 
+        {
+            SetScript = {
+                $BootParams = $using:BootParameters
+                Import-Module -Name rsBoot -Force
+
+                Get-ChildItem -Path Cert:\LocalMachine\My\ |
+                Where-Object -FilterScript {$_.Subject -eq "CN=$($BootParams.PullServerAddress)"} | 
+                Remove-Item
+                
+                $EndDate = (Get-Date).AddYears(25) | Get-Date -Format MM/dd/yyyy
+                New-SelfSignedCertificateEx -Subject "CN=$($BootParams.PullServerAddress)" -NotAfter $EndDate -StoreLocation LocalMachine -StoreName My -Exportable -KeyLength 2048
+            }
+            TestScript = {
+                $BootParams = $using:BootParameters
+                if( Get-ChildItem -Path Cert:\LocalMachine\My\ | Where-Object -FilterScript {$_.Subject -eq "CN=$($BootParams.PullServerAddress)"} ) 
+                {
+                    return $true
+                }
+                else 
+                {
+                    return $false
+                }
+            }
+            GetScript = {
+                $BootParams = $using:BootParameters
+                return @{
+                    'Result' = (Get-ChildItem -Path Cert:\LocalMachine\My\ | Where-Object -FilterScript {$_.Subject -eq "CN=$($BootParams.PullServerAddress)"}).Thumbprint
+                }
+            }
+        }
+        WindowsFeature IIS 
+        {
+            Ensure = 'Present'
+            Name = 'Web-Server'
+        }
+        WindowsFeature DSCServiceFeature 
+        {
+            Ensure = 'Present'
+            Name = 'DSC-Service'
+            DependsOn = '[WindowsFeature]IIS'
+        }
+        Script InstallRootCertificate 
+        {
+            SetScript = {
+                $BootParams = $using:BootParameters
+                Get-ChildItem -Path Cert:\LocalMachine\Root\ |
+                Where-Object -FilterScript {$_.Subject -eq "CN=$($BootParams.PullServerAddress)"} |
+                Remove-Item
+                $store = Get-Item Cert:\LocalMachine\Root
+                $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]'ReadWrite')
+                $store.Add( $(New-Object System.Security.Cryptography.X509Certificates.X509Certificate -ArgumentList @(,(Get-ChildItem Cert:\LocalMachine\My | Where-Object Subject -eq "CN=$($BootParams.PullServerAddress)").RawData)) )
+                $store.Close()
+            }
+            TestScript = {
+                $BootParams = $using:BootParameters
+                if((Get-ChildItem -Path Cert:\LocalMachine\Root\ | Where-Object -FilterScript {$_.Subject -eq "CN=$($BootParams.PullServerAddress)"}).Thumbprint -eq (Get-ChildItem -Path Cert:\LocalMachine\My\ | Where-Object -FilterScript {$_.Subject -eq "CN=$($BootParams.PullServerAddress)"}).Thumbprint) 
+                {
+                    return $true
+                }
+                else 
+                {
+                    return $false
+                }
+            }
+            GetScript = {
+                $BootParams = $using:BootParameters
+                return @{
+                    'Result' = (Get-ChildItem -Path Cert:\LocalMachine\Root\ | Where-Object -FilterScript {$_.Subject -eq "CN=$($BootParams.PullServerAddress)"}).Thumbprint
+                }
+            }
+            DependsOn = '[Script]CreateServerCertificate'
+        }
+    } 
+}
 
 #endregion
 
@@ -66,10 +327,10 @@ Write-Verbose "Setting environment variables"
 Write-Verbose " - DefaultPath location: $DefaultPath"
 Write-Verbose " - NodeInfoPath location: $NodeInfoPath"
 
-if (-not(Test-Path $defaultPath))
+if (-not(Test-Path $DefaultPath))
 {
-    Write-Verbose "Creating configuration directory: $defaultPath"
-    New-Item -Path $defaultPath -ItemType Directory
+    Write-Verbose "Creating configuration directory: $DefaultPath"
+    New-Item -Path $DefaultPath -ItemType Directory
 }
 else
 {
@@ -81,7 +342,7 @@ Write-Verbose "Setting folder permissions for $DefaultPath"
 #Disable persmission inheritance on $defaultPath
 $objACL = Get-ACL -Path $defaultPath
 $objACL.SetAccessRuleProtection($True, $True)
-Set-ACL $defaultPath $objACL
+Set-ACL $DefaultPath $objACL
 
 # Remove BUILTIN\Users access to $defaultPath
 $colRights = [System.Security.AccessControl.FileSystemRights]"ReadAndExecute" 
@@ -155,7 +416,6 @@ if (-not (Test-Connection $Target -Quiet))
     until (-not (Test-Connection $Target -Quiet))
 }
 
-<#
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 $WinTemp = "$env:windir\Temp"
 $ModuleFileName = $BootModuleZipURL.Split("/")[-1]
@@ -186,7 +446,6 @@ if (Test-Path "$PSModuleLocation\$ModuleName")
 }
 Write-Verbose "Installing rsBoot module"
 Move-Item -Path "$WinTemp\$ModuleName" -Destination $PSModuleLocation
-#>
 Write-Verbose "Importing $ModuleName module"
 Import-Module -Name $ModuleName -Force -Verbose
 #endregion
@@ -214,13 +473,18 @@ if ($PullServerConfig -ne $null)
     # Need $pullserver_config parameter/variable
     Write-Secrets -PullServerAddress $PullServerAddress -pullserver_config $PullServerConfig `
                   -BootParameters $BootParameters -Path $DefaultPath
+    
+    Write-Verbose "Configuring WinRM"
     Enable-WinRM
 
+    Write-Verbose "Starting Pull Server Boot DSC configuration run"
+    PullBoot -BootParameters $BootParameters `
+             -PullConfigInstallPath $DefaultPath `
+             -OutputPath (Join-Path $DefaultPath -ChildPath DSCboot)
+
+    Start-DscConfiguration -Path (Join-Path $DefaultPath -ChildPath DSCboot) -Wait -Verbose -Force
+
     <#
-    
-    
-    # Boot -PullServerAddress $PullServerAddress -OutputPath $defaultPath -Verbose
-    # Start-DscConfiguration -Force -Path $defaultPath -Wait -Verbose
     Set-LCM
     #PullServer - Run RsPlatform and then PullServer config file
     if($secrets){
