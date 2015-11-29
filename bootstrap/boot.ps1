e4b467ba204ea64ed6648153638a96bccc3e3b17
Alexei Andreyev <alexei.andreyev@rackspace.co.uk>
2015-11-29 22:35:37 +0000

    Remove redundant functions

--- bootstrap/boot.ps1
+++ bootstrap/boot.ps1
@@ -848,92 +848,6 @@ function Install-PlatformModules
 #########################################################################################################
 # Helper functions
 #region##################################################################################################
-function Write-DSCAutomationSettings 
-{
-    [CmdletBinding()]
-    param
-    (
-        # Destination path for DSC Automation secure settings file
-        [string]
-        $Path = (Join-Path $env:defaultPath "DSCAutomationSettings.xml"),
-
-        # Certificate hash with which to ecrypt the settigns
-        [Parameter(Mandatory=$true)]
-        [string]
-        $CertThumbprint,
-
-        # Contents of the settings file
-        [Parameter(Mandatory=$true)]
-        [hashtable]
-        $Settings
-    )
-
-    # Create the certificate object whith which to secure the data
-    $CertObject = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Thumbprint -like $CertThumbprint}
-
-    # Process the provided hashtable and encrypt just the value string for each KV pair
-    $DSCAutomationSettings = @{}
-    $Settings.GetEnumerator() | Foreach {
-        $EncodedValue = [system.text.encoding]::UTF8.GetBytes($_.Value)
-        $EncryptedBytes = $CertObject.PublicKey.Key.Encrypt($EncodedValue, $true)
-        $EncryptedValue = [System.Convert]::ToBase64String($EncryptedBytes)
-        $DSCAutomationSettings.Add($($_.Key),$EncryptedValue)
-    }
-
-    # Adding cert hash value to databag to make it easier to decrypt the data later
-    $DSCAutomationSettings.Add("CertThumbprint",$CertThumbprint)
-    
-    # Make a backup in case of there being an existing settings file
-    if (Test-Path $Path)
-    {
-        Write-Verbose "Existing settings file found - making a backup..."
-        $TimeDate = (Get-Date -Format ddMMMyyyy_hhmmss).ToString()
-        Move-Item $Path -Destination ("$Path`-$TimeDate.bak") -Force
-    }
-    
-    # Save the encrypted databag as a native PS hashtable object
-    $DSCAutomationSettings | Export-Clixml -Path $Path -Force
-}
-
-function Read-DSCAutomationSettings 
-{
-    [CmdletBinding()]
-    param
-    (
-        # Source path for the secure settings file
-        [string]
-        $Path = (Join-Path $env:defaultPath "DSCAutomationSettings.xml"),
-
-        # Certificate hash with which to decrypt the settigns
-        [string]
-        $CertThumbprint
-    )
-
-    Write-Verbose "Importing the settings databag"
-    $EncrytedSettings = Import-Clixml -Path $Path
-
-    # Try to locate the the original cert using the hash in databag if one is not provided
-    if (-not $CertThumbprint)
-    {
-        $CertThumbprint = $EncrytedSettings.CertThumbprint
-    }
-    # We will not need the thumbprint as part the returned settings
-    $EncrytedSettings.Remove("CertThumbprint")
-
-    # Create the certificate object whith which to decrypt the data
-    $CertObject = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Thumbprint -like $CertThumbprint}
-
-    # Process the provided hashtable and decrypt just the value string for each KV pair
-    $DecryptedSettings = @{}
-    $EncrytedSettings.GetEnumerator() | Foreach {
-        $EncryptedBytes = [System.Convert]::FromBase64String($_.Value)
-        $DecryptedBytes = $CertObject.PrivateKey.Decrypt($EncryptedBytes, $true)
-        $DecryptedValue = [system.text.encoding]::UTF8.GetString($DecryptedBytes)
-        $DecryptedSettings.Add($($_.Key),$DecryptedValue)
-    }
-    $DecryptedSettings
-}
-#endregion
 
 #########################################################################################################
 # Create a task to persist bootstrap accross reboots
@@ -1032,8 +946,6 @@ $BootParameters = @{}
         }
     }
 
-#$BootParameters | Export-Clixml -Path "$InstallPath\settings.xml" -Force
-
 # Determine if we're building a Pull server or a client
 if ($PullServerConfig)
 {
@@ -1071,8 +983,9 @@ if ($PullServerConfig)
     $CertThumbprint = (Get-ChildItem Cert:\LocalMachine\My | 
                         Where-Object -FilterScript {$_.Subject -eq "CN=$PullServerAddress"}).Thumbprint
     
-    # Procecss bootstrap parameters and save only the ones we need to config file
-    $SettingKeyFilterSet = @("SharedKey",
+    # Procecss bootstrap parameters and save only settings we need to commit to config file
+    $SettingKeyFilterSet = @(
+                             "SharedKey",
                              "InstallPath",
                              "GitRepoName",
                              "GitOrgName",
@@ -1083,7 +996,6 @@ if ($PullServerConfig)
                              "PullServerConfig",
                              "PullServerAddress"
                             )
-    
     $DSCSettings = @{}
     $BootParameters.GetEnumerator() | foreach {  
         if ($SettingKeyFilterSet -contains $($_.Name))
